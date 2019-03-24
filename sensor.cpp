e5cf364ffec4ad7b4bb94a2d51f9d081b4fcb329
Jakub Martenka <j.martenka@combo-gaming.com>
2019-03-24 03:05:51 +0100

    DHT sensors support

--- sensor.cpp
+++ sensor.cpp
@@ -1,25 +1,58 @@
 #include "sensor.h"
+#include <DHT.h>
 
-Sensor::Sensor(const SensorDef* def)
-: def(def){
-	pinMode(def->pin, INPUT);
+DHT dht(2, DHT22);
+
+
+Sensor::Sensor(const SensorDef* def): def(def) {
+  this->def = def;
+
+  if (this->def->type == SENSOR_ANALOG)
+  {
+    pinMode(def->pin, INPUT);
+  }
+
+  if (this->def->type == SENSOR_DHT22)
+  {
+    dht.begin();
+  }
 }
 
 
 double Sensor::rawTemp() const {
-	double tempLog = log(10000.0 * (1024.0 / analogRead(def->pin) - 1));
-	return 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * tempLog * tempLog ))* tempLog ) - 273.15;
+  if (this->def->type == SENSOR_ANALOG)
+  {
+    double tempLog = log(10000.0 * (1024.0 / analogRead(def->pin) - 1));
+    return 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * tempLog * tempLog )) * tempLog ) - 273.15;
+  }
+
+  if (this->def->type == SENSOR_DHT22)
+  {
+    float t = dht.readTemperature();
+
+    if (isnan(t))
+    {
+      Serial.println("Failed to read from DHT");
+    }
+    else
+    {
+      return (t);
+    }
+
+    return this->def->sensorFailTemp;
+  }
 }
 
 
 double Sensor::smoothedTemp() const {
-	return temp;
+  return temp;
 }
 
-void Sensor::measureTemp(){
-	temp = (temp * (double)counter + rawTemp()) / ((double)counter + 1.0);
-	counter++;
+void Sensor::measureTemp() {
+  temp = (temp * (double)counter + rawTemp()) / ((double)counter + 1.0);
+  counter++;
 }
-void Sensor::resetTemp(){
-	counter = 0;
+
+void Sensor::resetTemp() {
+  counter = 0;
 }
--- sensor.h
+++ sensor.h
@@ -3,32 +3,36 @@
 
 #include <Arduino.h>
 
+#define SENSOR_ANALOG 1
+#define SENSOR_DHT22 2
 
-struct SensorDef{
-	char* name;
-	uint8_t pin;
-	double maxTemp;
+
+struct SensorDef {
+  char* name;
+  uint8_t pin;
+  double maxTemp;
+  double sensorFailTemp;
+  uint8_t type;
 };
 
 
 class Sensor {
-public:
-	Sensor(){}
-	Sensor(const SensorDef* def);
+  public:
+    Sensor() {}
+    Sensor(const SensorDef* def);
 
-	double rawTemp() const;
+    double rawTemp() const;
+    double smoothedTemp() const;
 
+    void measureTemp();
+    void resetTemp();
 
-	double smoothedTemp() const;
+    const SensorDef* def;
 
-	void measureTemp();
-	void resetTemp();
+  private:
+    double temp = 0.0;
+    uint16_t counter = 0;
 
-	const SensorDef* def;
-
-private:
-	double temp = 0.0;
-	uint16_t counter = 0;
 };
 
 #endif
